03a9e2b4c20d09e864539b7a40e7f5fa00198fa5
Martin Andronikos <martin.andronikos@gmail.com>
2024-09-17 00:19:12 +0300

    Preliminary IO FIFO
    Added a non-blocking FIFO implementation for passing
    data between a producer and a consumer thread in the
    context of an IO device interacting with the CPU.

--- include/core/io.h
+++ include/core/io.h
@@ -5,6 +5,9 @@
 #include <stdbool.h>
 
 #define IO_ADDR_BITS 8
+#define IO_FIFO_SIZE_BITS 8
+
+// IO Registry Section //
 
 typedef void (*io_callback_t)(bool rw_select, uint32_t *data);
 typedef struct io_registry *io_t;
@@ -19,5 +22,16 @@ bool io_detach_write(io_t io, uint16_t port, io_callback_t *ret_callback);
 
 uint32_t io_read(io_t io, uint16_t port);
 void io_write(io_t io, uint16_t port, uint32_t value);
+
+// IO FIFO Section //
+
+typedef struct io_fifo_object *io_fifo_t;
+
+io_fifo_t io_fifo_new(void);
+void io_fifo_del(io_fifo_t fifo);
+
+bool io_fifo_read(io_fifo_t fifo, uint32_t *data);
+bool io_fifo_write(io_fifo_t fifo, uint32_t *data);
+uint32_t io_fifo_space(io_fifo_t fifo);
 
 #endif // PPLUSEMU_CORE_IO_H
--- source/core/io.c
+++ source/core/io.c
@@ -2,8 +2,11 @@
 
 #include "util.h"
 
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+// IO Registry Section //
 
 typedef struct io_registry {
 	io_callback_t read_callbacks[(1 << IO_ADDR_BITS)];
@@ -67,4 +70,57 @@ void io_write(io_t io, uint16_t port, uint32_t value) {
 		return;
 	}
 	callback(true, &value);
+}
+
+// IO FIFO Section //
+
+typedef struct io_fifo_object {
+	pthread_mutex_t mutex;
+	volatile uint32_t widx, ridx;
+	volatile bool full, empty;
+	volatile uint32_t buffer[];
+} io_fifo_object_t;
+
+io_fifo_t io_fifo_new(void) {
+	size_t size = sizeof(io_fifo_object_t);
+	size += (1 << IO_FIFO_SIZE_BITS) * sizeof(uint32_t);
+	io_fifo_t fifo = (io_fifo_t) calloc(1, size);
+	if(fifo->buffer == NULL) return NULL;
+	if(pthread_mutex_init(&fifo->mutex, NULL) != 0) return free(fifo), NULL;
+	fifo->widx = fifo->ridx = 0, fifo->empty = true, fifo->full = false;
+	return fifo;
+}
+
+void io_fifo_del(io_fifo_t fifo) {
+	pthread_mutex_trylock(&fifo->mutex);
+	pthread_mutex_unlock(&fifo->mutex);
+	pthread_mutex_destroy(&fifo->mutex);
+	free(fifo);
+}
+
+bool io_fifo_read(io_fifo_t fifo, uint32_t *data) {
+	if(fifo->empty) return false;
+	pthread_mutex_lock(&fifo->mutex);
+	*data = fifo->buffer[fifo->ridx];
+	fifo->ridx = (fifo->ridx + 1) & ((1 << IO_FIFO_SIZE_BITS) - 1);
+	uint32_t space = (fifo->widx - fifo->ridx) & ((1 << IO_FIFO_SIZE_BITS) - 1);
+	fifo->empty = (space == 0), fifo->full = false;
+	pthread_mutex_unlock(&fifo->mutex);
+	return true;
+}
+
+bool io_fifo_write(io_fifo_t fifo, uint32_t *data) {
+	if(fifo->full) return false;
+	pthread_mutex_lock(&fifo->mutex);
+	fifo->buffer[fifo->widx] = *data;
+	uint32_t space = (fifo->widx - fifo->ridx) & ((1 << IO_FIFO_SIZE_BITS) - 1);
+	fifo->empty = false, fifo->full = space == ((1 << IO_FIFO_SIZE_BITS) - 1);
+	fifo->widx = (fifo->widx + 1) & ((1 << IO_FIFO_SIZE_BITS) - 1);
+	pthread_mutex_unlock(&fifo->mutex);
+	return true;
+}
+
+uint32_t io_fifo_space(io_fifo_t fifo) {
+	uint32_t space = 1 << IO_FIFO_SIZE_BITS;
+	return space - fifo->widx + fifo->ridx;
 }

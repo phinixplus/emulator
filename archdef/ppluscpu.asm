7380f6e4c84b17384feea49628b20f271959f00d
Martin Andronikos <martin.andronikos@gmail.com>
2025-06-21 00:19:10 +0300

    The first few ALU instructions
    I know they're not that many and I know that I've been gone
    for a while, but at least I'm happy to have the time for this again.
    I'm working behind the scenes with the docs to organize
    the instructions in a better way before they end up here.

--- archdef/ppluscpu.asm
+++ archdef/ppluscpu.asm
@@ -37,33 +37,62 @@
 }
 
 #subruledef cond {
-	c0 => 0x0`3
-	c1 => 0x1`3
-	c2 => 0x2`3
-	c3 => 0x3`3
-	c4 => 0x4`3
-	c5 => 0x5`3
-	c6 => 0x6`3
-	c7 => 0x7`3
+	c0 => 0x0`4
+	c1 => 0x1`4
+	c2 => 0x2`4
+	c3 => 0x3`4
+	c4 => 0x4`4
+	c5 => 0x5`4
+	c6 => 0x6`4
+	c7 => 0x7`4
+	!{base: cond} =>
+		({base} ^ 8)`4
 }
 
 ; ---------------------------------------------------------------------------- ;
 
-#subruledef ncond {
-	  {base: cond}  => 0`1 @ {base}
-	 !{base: cond}  => 1`1 @ {base}
-	!!{base: ncond} => {base}
+#subruledef flag {
+	C => 0`2
+	K => 1`2
+	!{base: flag} =>
+		({base} ^ 2)`2
+}
+
+#subruledef mix {
+	ior => 0`1
+	and => 1`1
 }
 
 #fn w_cc3g(funct, tgt_g, src1_g, src2_g, nt, tgt_c, ns, src_c) =>
 	src1_g`4 @ tgt_g`4 @ src2_g`4 @ funct`4 @ ns`1 @ src_c`3 @ nt`1 @ tgt_c`3
-#fn w_g_il(tgt_g, imm20) => imm20[2:16] @ tgt_g`4 @ imm20[15:0]
+#fn w_g_il(tgt_g, imm20) => imm20[19:16] @ tgt_g`4 @ imm20[15:0]
 #fn w_gg_ih(tgt_g, src_g, imm16) => src_g`4 @ tgt_g`4 @ imm16`16
 
 ; ---------------------------------------------------------------------------- ;
 
+#ruledef native_wg_il {
+	jnl {dg: data} [ ip {lbl: u32} ] => {
+		rel = ({lbl} - $) >> 1
+		assert($[0:0] == 0 && {lbl}[0:0] == 0, "Misaligned jump target.")
+		assert(rel[31:20] == 0 || rel[31:20] == 0xFFF, "Jump target out of range.")
+		0x10`8 @ w_g_il({dg}, rel)
+	}
+}
+
 #ruledef native_w_gg_ih {
 	inp {dg: data} [ {sg: data} {ih: u16} ] => 0x20`8 @ w_gg_ih({dg}, {sg}, {ih})
 	out {dg: data} [ {sg: data} {ih: u16} ] => 0x21`8 @ w_gg_ih({dg}, {sg}, {ih})
+
+	add {dg: data} {sg: data} {ih: i16} => 0x22`8 @ w_gg_ih({dg}, {sg}, {ih})
 }
 
+#ruledef native_w_cc3g {
+	add {dg: data} {s1g: data} {s2g: data}, {m: mix} {dc: cond} {sc: cond} {f: flag} => {
+		assert({dc}[3:3] == 0, "Unimplemented pseudo-instruction.")
+		0x40`8 @ w_cc3g({f}[0:0] @ {m}, {dg}, {s1g}, {s2g}, {f}[1:1], {dc}, {sc}[3:3], {sc})
+	}
+	sub {dg: data} {s1g: data} {s2g: data}, {m: mix} {dc: cond} {sc: cond} {f: flag} => {
+		assert({dc}[3:3] == 0, "Unimplemented pseudo-instruction.")
+		0x41`8 @ w_cc3g({f}[0:0] @ {m}, {dg}, {s1g}, {s2g}, {f}[1:1], {dc}, {sc}[3:3], {sc})
+	}
+}
--- source/core/cpu.c
+++ source/core/cpu.c
@@ -53,6 +53,16 @@ static inline uint16_t get_imm_w_gg_ih(instruction_t *instr) {
 	return imm;
 }
 
+static inline bool get_cond(cpu_t *cpu, uint8_t pos) {
+	uint8_t tmp = EXTRACT(cpu->cond, 1, pos);
+	return tmp != 0;
+}
+
+static inline void put_cond(cpu_t *cpu, uint8_t pos, bool cond) {
+	cpu->cond &= ~MASK(1, pos);
+	cpu->cond |= (cond ? 1 : 0) << pos;
+}
+
 void cpu_new(cpu_t *cpu, mem_t mem, io_t io) {
 	// Make sure the instruction formats union is packed correctly.
 	assert(sizeof(instruction_t) == sizeof(uint32_t));
@@ -87,18 +97,62 @@ void cpu_execute(cpu_t *cpu) {
 	instruction_t instr = {inst_word};
 
 	switch(instr.opcode) {
-		uint32_t tmp_g1, tmp_g2;
+		uint32_t tmp1_g, tmp2_g;
+		bool tmp1_c, tmp2_c, tmp3_c;
+		case 0x10: // JNLil
+			cpu->data[instr.w_g_il.tgt_g] = cpu->ip + 4;
+			tmp1_g = get_imm_w_g_il(&instr);
+			tmp1_g |= EXTRACT(tmp1_g, 1, 19) != 0 ? MASK(12, 20) : 0;
+			cpu->ip += tmp1_g << 1;
+			break;
 		case 0x20: // INP
-			tmp_g1 = cpu->data[instr.w_gg_ih.src_g];
-			tmp_g1 += get_imm_w_gg_ih(&instr);
-			tmp_g2 = io_read(cpu->io, tmp_g1 & 0xFFFF);
-			cpu->data[instr.w_gg_ih.tgt_g] = tmp_g2;
+			tmp1_g = cpu->data[instr.w_gg_ih.src_g];
+			tmp1_g += get_imm_w_gg_ih(&instr);
+			tmp2_g = io_read(cpu->io, tmp1_g & 0xFFFF);
+			cpu->data[instr.w_gg_ih.tgt_g] = tmp2_g;
+
+			cpu->ip += 4;
 			break;
 		case 0x21: // OUT
-			tmp_g1 = cpu->data[instr.w_gg_ih.src_g];
-			tmp_g1 += get_imm_w_gg_ih(&instr);
-			tmp_g2 = cpu->data[instr.w_gg_ih.tgt_g];
-			io_write(cpu->io, tmp_g1 & 0xFFFF, tmp_g2);
+			tmp1_g = cpu->data[instr.w_gg_ih.src_g];
+			tmp1_g += get_imm_w_gg_ih(&instr);
+			tmp2_g = cpu->data[instr.w_gg_ih.tgt_g];
+			io_write(cpu->io, tmp1_g & 0xFFFF, tmp2_g);
+
+			cpu->ip += 4;
+			break;
+		case 0x22: // ADDih
+			tmp1_g = cpu->data[instr.w_gg_ih.src_g];
+			tmp1_g += (int16_t) get_imm_w_gg_ih(&instr);
+			cpu->data[instr.w_gg_ih.tgt_g] = tmp1_g;
+
+			cpu->ip += 4;
+			break;
+		case 0x40: // ADDr
+			tmp1_g = cpu->data[instr.w_cc3g.src1_g];
+			tmp2_g = tmp1_g + cpu->data[instr.w_cc3g.src2_g];
+			cpu->data[instr.w_cc3g.tgt_g] = tmp2_g;
+
+			tmp1_c = get_cond(cpu, instr.w_cc3g.src_c) ^ instr.w_cc3g.neg_sc;
+			tmp2_c = (tmp2_g < tmp1_g) ^ instr.w_cc3g.neg_tc;
+			tmp3_c = EXTRACT(instr.w_cc3g.funct, 1, 0);
+			tmp3_c = tmp3_c ? tmp1_c & tmp2_c : tmp1_c | tmp2_c;
+			put_cond(cpu, instr.w_cc3g.tgt_c, tmp3_c);
+
+			cpu->ip += 4;
+			break;
+		case 0x41: // SUBr
+			tmp1_g = cpu->data[instr.w_cc3g.src1_g];
+			tmp2_g = tmp1_g - cpu->data[instr.w_cc3g.src2_g];
+			cpu->data[instr.w_cc3g.tgt_g] = tmp2_g;
+
+			tmp1_c = get_cond(cpu, instr.w_cc3g.src_c) ^ instr.w_cc3g.neg_sc;
+			tmp2_c = (tmp2_g < tmp1_g) ^ instr.w_cc3g.neg_tc;
+			tmp3_c = EXTRACT(instr.w_cc3g.funct, 1, 0);
+			tmp3_c = tmp3_c ? tmp1_c & tmp2_c : tmp1_c | tmp2_c;
+			put_cond(cpu, instr.w_cc3g.tgt_c, tmp3_c);
+
+			cpu->ip += 4;
 			break;
 		default:
 			stop_running();
